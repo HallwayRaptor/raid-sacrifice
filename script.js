c2878dff372f8804f9046873763cd85fcf25b8fa
Rob Whelan <hallwayraptor@gmail.com>
2022-03-21 14:31:14 -0300

    add ui

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const raidersList = document.getElementById(`raiders`);
+const consoleBox = document.querySelector(`.console`);
+const sacrificeButton = document.getElementById(`sacrifice`);
+
 let raiders = [
 	`Rob`,
 	`Kevin`,
@@ -8,8 +12,28 @@ let raiders = [
 	`Mike`,
 	`Scarlett`,
 ];
+
+let data = ["Ram", "Shyam", "Sita", "Gita"];
+
+let list = document.getElementById("myList");
+
+raiders.forEach(item => {
+	let li = document.createElement("li");
+	li.innerText = item;
+	list.appendChild(li);
+});
+
+/*
 for (let i = 0; i < raiders.length; i++) {
 	let choice = Math.floor(Math.random() * raiders.length);
-	console.log(`You sacrifice ${raiders[choice]} to Zodiark`);
+	consoleBox.textContent = `You sacrifice ${raiders[choice]} to Zodiark`;
 	raiders.splice(choice, 1);
 }
+*/
+function sacrifice() {
+	let choice = Math.floor(Math.random() * raiders.length);
+	consoleBox.textContent = `You sacrifice ${raiders[choice]} to Zodiark`;
+	// raiders.splice(choice, 1);
+}
+
+sacrificeButton.addEventListener(`click`, sacrifice);
